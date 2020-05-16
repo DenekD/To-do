75a0dc230b7e8c17437e40f945eed05925f578d1
Barnaba <Denek20@gmail.com>
2020-05-16 12:14:13 +0200

    Updates

---
+++ precache-manifest.12a281a8e55dbfb65fd235c51004cdaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6489ebd192a5d1e5e96d0a73d59a2aac",
+    "url": "/To-do/index.html"
+  },
+  {
+    "revision": "0721000a17dfc9cc669b",
+    "url": "/To-do/static/css/main.75e7fca0.chunk.css"
+  },
+  {
+    "revision": "2d7d858ea89cbd084eb3",
+    "url": "/To-do/static/js/2.ef6c0639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do/static/js/2.ef6c0639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0721000a17dfc9cc669b",
+    "url": "/To-do/static/js/main.e55b7c5e.chunk.js"
+  },
+  {
+    "revision": "6913695ac3048e55af1d",
+    "url": "/To-do/static/js/runtime-main.cb9e3cc4.js"
+  },
+  {
+    "revision": "3f307db3432acccec34dd3c809ece3a6",
+    "url": "/To-do/static/media/checkedIcon.3f307db3.svg"
+  },
+  {
+    "revision": "b9e65c667436d5b68b4d286eebd07861",
+    "url": "/To-do/static/media/desk.b9e65c66.jpg"
+  },
+  {
+    "revision": "87da38651727cea7140bf328302a5c61",
+    "url": "/To-do/static/media/removeIcon.87da3865.svg"
+  }
+]);
