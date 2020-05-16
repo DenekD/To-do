ef13063bcb623a0d890da7b98f0126c5158587e9
Barnaba <Denek20@gmail.com>
2020-05-16 11:48:11 +0200

    my first repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Input from './Components/Input';
+import './App.css';
+import ToDoList from './Components/ToDoList';
+import CompletedList from './Components/CompletedList';
+
+
+class App extends Component {
+
+  state = {
+    tasks: [
+      {
+        text: "kup głosnik",
+        id: 0,
+        completed: false
+      },
+      {
+        text: "wyrzuć śmieci",
+        id: 1,
+        completed: false
+      },
+      {
+        text: "zrób obiad",
+        id: 2,
+        completed: true
+      },
+    ]
+  }
+
+  compliteTask = (id) => {
+    this.setState({
+      tasks: this.state.tasks.map( task => {
+        if( id === task.id) {
+          return {
+            ...task,
+            completed: true
+          }
+        }
+        return task;
+      })
+    })
+  }
+
+  removeTask = (id) =>
+    this.setState({
+      tasks: this.state.tasks.filter( task => id !== task.id)
+  });
+
+  id = 4;
+
+  addTask = text => {
+    this.setState({
+      tasks: [
+        {
+          text,
+          id: this.id += 1,
+          completed: false
+        },
+        ...this.state.tasks
+      ]
+    })
+  }
+
+  render () {
+    return (
+      <div className="wrapper">
+        <Input addTask={this.addTask} />
+        <ToDoList
+          tasks={this.state.tasks}
+          compliteTask={this.compliteTask}
+          removeTask={this.removeTask}
+        />
+        <CompletedList
+          tasks={this.state.tasks}
+          removeTask={this.removeTask}
+        />
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
